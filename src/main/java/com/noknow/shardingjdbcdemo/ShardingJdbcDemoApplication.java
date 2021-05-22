a83710335093563d8b263e4823c8ce7746b517ad
wang.jianwen <1272657070@qq.com>
2021-05-22 15:31:43 +0800

    init

---
+++ src/main/java/com/noknow/shardingjdbcdemo/ShardingJdbcDemoApplication.java
@@ -0,0 +1,23 @@
+package com.noknow.shardingjdbcdemo;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.shardingsphere.shardingjdbc.spring.boot.SpringBootConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.FilterType;
+
+@SpringBootApplication(exclude = {
+    DataSourceAutoConfiguration.class, SpringBootConfiguration.class
+})
+@ComponentScan(basePackages = "com.noknow", excludeFilters = {
+    @Filter(type = FilterType.ASSIGNABLE_TYPE, classes = Mapper.class)})
+public class ShardingJdbcDemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ShardingJdbcDemoApplication.class, args);
+  }
+
+}
